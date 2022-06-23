5c8cfc0093c9618645887b0db55d00a2f98355f4
Barny victor <victorbarny4@gmail.com>
2022-06-23 10:06:56 +0100

    changed some things

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ let Operator = prompt("Please Add an operator +,*,-,/");
 function Calculator(input1, input2, Operator) {
   if (Operator == "+") {
     let Answer = input1 + input2;
-    alert(Answer);
+    alert(`Answer:${Answer}`);
   } else if (Operator == "-") {
     let Answer = input1 - input2;
-    alert(Answer);
+    alert(`Answer:${Answer}`);
   } else if (Operator == "*") {
     let Answer = input1 * input2;
-    alert(Answer.toFixed(2));
+    alert(`Answer:${Answer.toFixed(2)}`);
   } else {
     let Answer = input1 / input2;
-    alert(Answer.toFixed(2));
+    alert(`Answer:${Answer.toFixed(2)}`);
   }
 }
 
